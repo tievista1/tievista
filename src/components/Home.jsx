926b6e969e8ca236e4f9eedbd9b3e32121fb8ddf
Krishnakant Mane <krishnamane712c@gmail.com>
2026-03-11 14:29:51 +0530

    Service, B2b, NRI, Footer,vercel route, Cursor,Title, removed Italic,Reduce Speed, Removed Numeric, All things Black, B2B to TieVista patners,ContactPage, LinkTo, LazyLoading, FallbackUI, preLoader in VideoTag, VideoQuality and HTML Tag ,FallBack in Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -129,7 +129,7 @@ const Home = () => {
                                     <img src="/icon.png" className="h-full w-full animate-pulse" alt="Icon" />
                                 </div>
                                 <span className="text-[#c9a36b] tracking-[0.4em] uppercase text-[10px] animate-pulse">
-                                    TieVistaHome
+                                    TieVista
                                 </span>
                             </motion.div>
                         </div>
