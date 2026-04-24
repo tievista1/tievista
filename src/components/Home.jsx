6f89f2c084ad35c5d45651059ee0c94337715379
KrishnaMane <krishnamane152@gmail.com>
2026-04-24 18:04:30 +0530

    Images Change and password add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
 
 
     const containerVariants = {
-        hidden: { opacity: 0, y: 20 },
+        hidden: { opacity: 0, y: 10 },
         visible: {
             opacity: 1,
             y: 0,
@@ -28,7 +28,7 @@ const Home = () => {
     };
 
     const itemVariants = {
-        hidden: { opacity: 0, y: 5 },
+        hidden: { opacity: 0, y: 10 },
         visible: { opacity: 1, y: 0 }
     };
 
@@ -42,7 +42,7 @@ const Home = () => {
                 {/* Hero Snap Scroll Section */}
                 <div className="h-[100vh] w-full snap-start relative flex items-center justify-center overflow-hidden border-b border-gray-100">
 
-                    <img className='absolute inset-0 w-full h-full object-cover z-0 transition-opacity duration-700' src="https://res.cloudinary.com/dck5jgfix/image/upload/v1774247093/Landingpage_ixd1ea.png" alt="LandingPage" loading='lazy' />
+                    <img className='absolute inset-0 w-full h-full object-cover z-0 transition-opacity duration-700' src="https://res.cloudinary.com/dck5jgfix/image/upload/v1777033664/HomeBG_jbaa4u.png" alt="LandingPage" loading='lazy' />
 
 
                     {/* Content */}
@@ -51,7 +51,7 @@ const Home = () => {
                         whileInView="visible"
                         viewport={{ once: false, amount: 0.5 }}
                         variants={containerVariants}
-                        className="relative h-full top-45 left-10 z-50   w-full"
+                        className="relative h-full top-60 left-10 z-50   w-full"
                     >
                         <motion.div
                             variants={itemVariants}
@@ -60,7 +60,7 @@ const Home = () => {
                         >
                             {/* <img className='w-100 h-25 md:h-28 lg:w-150 lg:h-35' src="/TieVistaVerticalLogo.png" alt="" /> */}
                             <div className='flex flex-col'>
-                                <h1 className='text-black font-light lg:text-7xl text-5xl '>Beyond Wealth, Towards Legacy</h1>
+                                <h1 className='text-white font-light lg:text-7xl text-5xl '>Beyond Wealth, Towards Legacy</h1>
                             </div>
 
                         </motion.div>
