45d7cea35d1bc7a96e6623ee45a24da084c2b2f8
Krishnakant Mane <krishnamane712c@gmail.com>
2026-07-08 13:33:59 +0530

    EUIN3

--- src/components/PartnersSignup.jsx
+++ src/components/PartnersSignup.jsx
@@ -1114,7 +1114,7 @@ const PatnersSignup = () => {
     const panRegex = /^[A-Z]{5}[0-9]{4}[A-Z]{1}$/;
     const dobRegex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
     const arnRegex = /^\d{6}$/;
-    const euinRegex = /^[A-Z]{1}\d{5}$/;
+    const euinRegex = /^[A-Z]{1}\d{6}$/;
     const aprnRegex = /^\d{5}$/;
 
     useEffect(() => {
